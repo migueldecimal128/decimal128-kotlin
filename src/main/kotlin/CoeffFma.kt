6e2b004646de6ab1801a3fbec0997bfea6bd0f93
MiguelDecimal128 <miguel@decimal128.com>
2025-05-11 06:21:20 -0400

    FMA name tweak

--- src/main/kotlin/CoeffFma.kt
+++ src/main/kotlin/CoeffFma.kt
@@ -25,12 +25,12 @@ object CoeffFma {
         val a0 = a.dw0
         val a1 = a.dw1
 
-        val maxProdBitLen = max(mBitLen + nBitLen, a.bitLen) + 1
+        val maxFusedBitLen = max(mBitLen + nBitLen, a.bitLen) + 1
         if (nBitLen <= 64) {
             when {
                 (mBitLen <= 64 && aBitLen <= 128) ->
                     _fma1x1x2(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         m0,
                         n0,
                         a1, a0
@@ -38,7 +38,7 @@ object CoeffFma {
 
                 (mBitLen <= 128 && aBitLen <= 192) ->
                     _fma2x1x3(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         m1, m0,
                         n0,
                         a.dw2, a1, a0
@@ -46,7 +46,7 @@ object CoeffFma {
 
                 (mBitLen <= 192) ->
                     _fma3x1x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         m.dw2, m1, m0,
                         n0,
                         a.dw3, a.dw2, a1, a0
@@ -54,7 +54,7 @@ object CoeffFma {
 
                 else ->
                     _fma4x1x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         m.dw3, m.dw2, m1, m0,
                         n0,
                         a.dw3, a.dw2, a1, a0
@@ -67,14 +67,14 @@ object CoeffFma {
             when {
                 (mBitLen <= 128) ->
                     _fma2x2x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         m1, m0,
                         n1, n0,
                         a.dw3, a.dw2, a1, a0
                     )
                 (mBitLen <= 192) ->
                     _fma3x2x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         m.dw2, m1, m0,
                         n1, n0,
                         a.dw3, a.dw2, a1, a0
@@ -98,30 +98,30 @@ object CoeffFma {
         val x0 = x.dw0
         val a0 = a.dw0
         val a1 = a.dw1
-        val maxProdBitLen = max(xBitLen + p10BitLen, aBitLen) + 1
+        val maxFusedBitLen = max(xBitLen + p10BitLen, aBitLen) + 1
         val p0 = POW10[pow10Offset + 0]
         if (p10BitLen <= 64) {
             when {
                 (xBitLen <= 64 && aBitLen <= 128) ->
-                    _fma1x1x2(z, maxProdBitLen,
+                    _fma1x1x2(z, maxFusedBitLen,
                         x0,
                         p0,
                         a1, a0
                     )
                 (xBitLen <= 128 && aBitLen <= 192) ->
-                    _fma2x1x3(z, maxProdBitLen,
+                    _fma2x1x3(z, maxFusedBitLen,
                         x.dw1, x0,
                         p0,
                         a.dw2, a1, a0
                     )
                 (xBitLen <= 192) ->
-                    _fma3x1x4(z, maxProdBitLen,
+                    _fma3x1x4(z, maxFusedBitLen,
                         x.dw2, x.dw1, x0,
                         p0,
                         a.dw3, a.dw2, a1, a0
                     )
                 else ->
-                    _fma4x1x4(z, maxProdBitLen,
+                    _fma4x1x4(z, maxFusedBitLen,
                         x.dw3, x.dw2, x.dw1, x0,
                         p0,
                         a.dw3, a.dw2, a1, a0)
@@ -133,21 +133,21 @@ object CoeffFma {
             when {
                 (xBitLen <= 64 && aBitLen <= 128) ->
                     _fma2x1x3(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         p1, p0,
                         x0,
                         a.dw2, a1, a0
                     )
                 (xBitLen <= 128) ->
                     _fma2x2x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         x.dw1, x0,
                         p1, p0,
                         a.dw3, a.dw2, a1, a0
                     )
                 (xBitLen <= 192) ->
                     _fma3x2x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         x.dw2, x.dw1, x0,
                         p1, p0,
                         a.dw3, a.dw2, a1, a0
@@ -161,14 +161,14 @@ object CoeffFma {
             when {
                 (xBitLen <= 64) ->
                     _fma3x1x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         p2, p1, p0,
                         x0,
                         a.dw3, a.dw2, a1, a0
                     )
                 (xBitLen <= 128) ->
                     _fma3x2x4(
-                        z, maxProdBitLen,
+                        z, maxFusedBitLen,
                         p2, p1, p0,
                         x.dw1, x0,
                         a.dw3, a.dw2, a1, a0
@@ -180,7 +180,7 @@ object CoeffFma {
         val p3 = POW10[pow10Offset + 3]
         if (xBitLen <= 64) {
             _fma4x1x4(
-                z, maxProdBitLen,
+                z, maxFusedBitLen,
                 p3, p2, p1, p0,
                 x0,
                 a.dw3, a.dw2, a1, a0
@@ -194,14 +194,14 @@ object CoeffFma {
     @Suppress("UNUSED")
     private fun _fma4x4x4(
         f: Coeff,
-        maxBitLen: Int,
+        maxFusedBitLen: Int,
         x3: Long, x2: Long, x1: Long, x0: Long,
         y3: Long, y2: Long, y1: Long, y0: Long,
         a3: Long, a2: Long, a1: Long, a0: Long
     ) {
         val pp00Hi = unsignedMultiplyHigh(x0, y0)
         val pp00Lo = x0 * y0
-        if (maxBitLen <= 64) {
+        if (maxFusedBitLen <= 64) {
             val f0 = pp00Lo + a0
             f.coeffSet64(f0)
             return
@@ -211,7 +211,7 @@ object CoeffFma {
         val pp01Lo = x0 * y1
         val pp10Hi = unsignedMultiplyHigh(x1, y0)
         val pp10Lo = x1 * y0
-        if (maxBitLen <= 128) {
+        if (maxFusedBitLen <= 128) {
             val f1 = carry0 + pp00Hi + pp01Lo + pp10Lo + a1
             f.coeffSet128(f1, f0)
             return
@@ -223,7 +223,7 @@ object CoeffFma {
         val pp02Lo = x0 * y2
         val pp20Hi = unsignedMultiplyHigh(x2, y0)
         val pp20Lo = x2 * y0
-        if (maxBitLen <= 192) {
+        if (maxFusedBitLen <= 192) {
             val f2 = carry1 + pp01Hi + pp10Hi + pp11Lo + pp02Lo + pp20Lo + a2
             f.coeffSet192(f2, f1, f0)
             return
@@ -238,7 +238,7 @@ object CoeffFma {
         val pp30Hi = unsignedMultiplyHigh(x3, y0)
         val pp30Lo = x3 * y0
 
-        if (maxBitLen <= 256) {
+        if (maxFusedBitLen <= 256) {
             val f3 = carry2 + pp11Hi + pp02Hi + pp20Hi + pp12Lo + pp21Lo + pp03Lo + pp30Lo + a3
             f.coeffSet256(f3, f2, f1, f0)
             return
@@ -247,7 +247,7 @@ object CoeffFma {
         val pp22Lo = x2 * y2
         val (carry4, f4) = sumU64(carry3, pp12Hi, pp21Hi, pp03Hi, pp30Hi, pp22Lo)
         if ((carry4 or f4) == 0L) {
-            assert(maxBitLen in 257..258)
+            assert(maxFusedBitLen in 257..258)
             f.coeffSet256(f3, f2, f1, f0)
             return
         }
@@ -256,14 +256,14 @@ object CoeffFma {
 
     private fun _fma4x1x4(
         f: Coeff,
-        maxBitLen: Int,
+        maxFusedBitLen: Int,
         x3: Long, x2: Long, x1: Long, x0: Long,
         y0: Long,
         a3: Long, a2: Long, a1: Long, a0: Long
     ) {
         val pp00Hi = unsignedMultiplyHigh(x0, y0)
         val pp00Lo = x0 * y0
-        if (maxBitLen <= 64) {
+        if (maxFusedBitLen <= 64) {
             val f0 = pp00Lo + a0
             f.coeffSet64(f0)
             return
@@ -271,7 +271,7 @@ object CoeffFma {
         val (carry0, f0) = sumU64(pp00Lo, a0)
         val pp10Hi = unsignedMultiplyHigh(x1, y0)
         val pp10Lo = x1 * y0
-        if (maxBitLen <= 128) {
+        if (maxFusedBitLen <= 128) {
             val f1 = carry0 + pp00Hi + pp10Lo + a1
             f.coeffSet128(f1, f0)
             return
@@ -279,7 +279,7 @@ object CoeffFma {
         val (carry1, f1) = sumU64(carry0, pp00Hi, pp10Lo, a1)
         val pp20Hi = unsignedMultiplyHigh(x2, y0)
         val pp20Lo = x2 * y0
-        if (maxBitLen <= 192) {
+        if (maxFusedBitLen <= 192) {
             val f2 = carry1 + pp10Hi + pp20Lo + a2
             f.coeffSet192(f2, f1, f0)
             return
@@ -288,7 +288,7 @@ object CoeffFma {
         val pp30Hi = unsignedMultiplyHigh(x3, y0)
         val pp30Lo = x3 * y0
 
-        if (maxBitLen <= 256) {
+        if (maxFusedBitLen <= 256) {
             val f3 = carry2 + pp20Hi + pp30Lo + a3
             f.coeffSet256(f3, f2, f1, f0)
             return
@@ -296,7 +296,7 @@ object CoeffFma {
         val (carry3, f3) = sumU64(carry2, pp20Hi, pp30Lo, a3)
         val (carry4, f4) = sumU64(carry3, pp30Hi)
         if ((carry4 or f4) == 0L) {
-            assert(maxBitLen in 257..258)
+            assert(maxFusedBitLen in 257..258)
             f.coeffSet256(f3, f2, f1, f0)
             return
         }
@@ -305,14 +305,14 @@ object CoeffFma {
 
     private fun _fma3x2x4(
         f: Coeff,
-        maxBitLen: Int,
+        maxFusedBitLen: Int,
         x2: Long, x1: Long, x0: Long,
         y1: Long, y0: Long,
         a3: Long, a2: Long, a1: Long, a0: Long
     ) {
         val pp00Hi = unsignedMultiplyHigh(x0, y0)
         val pp00Lo = x0 * y0
-        if (maxBitLen <= 64) {
+        if (maxFusedBitLen <= 64) {
             val f0 = pp00Lo + a0
             f.coeffSet64(f0)
             return
@@ -322,7 +322,7 @@ object CoeffFma {
         val pp01Lo = x0 * y1
         val pp10Hi = unsignedMultiplyHigh(x1, y0)
         val pp10Lo = x1 * y0
-        if (maxBitLen <= 128) {
+        if (maxFusedBitLen <= 128) {
             val f1 = carry0 + pp00Hi + pp01Lo + pp10Lo + a1
             f.coeffSet128(f1, f0)
             return
@@ -332,7 +332,7 @@ object CoeffFma {
         val pp11Lo = x1 * y1
         val pp20Hi = unsignedMultiplyHigh(x2, y0)
         val pp20Lo = x2 * y0
-        if (maxBitLen <= 192) {
+        if (maxFusedBitLen <= 192) {
             val f2 = carry1 + pp01Hi + pp10Hi + pp11Lo + pp20Lo + a2
             f.coeffSet192(f2, f1, f0)
             return
@@ -341,7 +341,7 @@ object CoeffFma {
         val pp21Hi = unsignedMultiplyHigh(x2, y1)
         val pp21Lo = x2 * y1
 
-        if (maxBitLen <= 256) {
+        if (maxFusedBitLen <= 256) {
             val f3 = carry2 + pp11Hi + pp20Hi + pp21Lo + a3
             f.coeffSet256(f3, f2, f1, f0)
             return
@@ -349,7 +349,7 @@ object CoeffFma {
         val (carry3, f3) = sumU64(carry2, pp11Hi, pp20Hi, pp21Lo, a3)
         val (carry4, f4) = sumU64(carry3, pp21Hi)
         if ((carry4 or f4) == 0L) {
-            assert(maxBitLen in 257..258)
+            assert(maxFusedBitLen in 257..258)
             f.coeffSet256(f3, f2, f1, f0)
             return
         }
@@ -358,14 +358,14 @@ object CoeffFma {
 
     private fun _fma3x1x4(
         f: Coeff,
-        maxBitLen: Int,
+        maxFusedBitLen: Int,
         x2: Long, x1: Long, x0: Long,
         y0: Long,
         a3: Long, a2: Long, a1: Long, a0: Long
     ) {
         val pp00Hi = unsignedMultiplyHigh(x0, y0)
         val pp00Lo = x0 * y0
-        if (maxBitLen <= 64) {
+        if (maxFusedBitLen <= 64) {
             val f0 = pp00Lo + a0
             f.coeffSet64(f0)
             return
@@ -373,7 +373,7 @@ object CoeffFma {
         val (carry0, f0) = sumU64(pp00Lo, a0)
         val pp10Hi = unsignedMultiplyHigh(x1, y0)
         val pp10Lo = x1 * y0
-        if (maxBitLen <= 128) {
+        if (maxFusedBitLen <= 128) {
             val f1 = carry0 + pp00Hi + pp10Lo + a1
             f.coeffSet128(f1, f0)
             return
@@ -381,21 +381,21 @@ object CoeffFma {
         val (carry1, f1) = sumU64(carry0, pp00Hi, pp10Lo, a1)
         val pp20Hi = unsignedMultiplyHigh(x2, y0)
         val pp20Lo = x2 * y0
-        if (maxBitLen <= 192) {
+        if (maxFusedBitLen <= 192) {
             val f2 = carry1 + pp10Hi + pp20Lo + a2
             f.coeffSet192(f2, f1, f0)
             return
         }
         val (carry2, f2) = sumU64(carry1, pp10Hi, pp20Lo, a2)
 
-        if (maxBitLen <= 256) {
+        if (maxFusedBitLen <= 256) {
             val f3 = carry2 + pp20Hi + a3
             f.coeffSet256(f3, f2, f1, f0)
             return
         }
         val (carry3, f3) = sumU64(carry2, pp20Hi, a3)
         if (carry3 == 0L) {
-            assert(maxBitLen in 257..258)
+            assert(maxFusedBitLen in 257..258)
             f.coeffSet256(f3, f2, f1, f0)
             return
         }
@@ -404,14 +404,14 @@ object CoeffFma {
 
     private fun _fma2x2x4(
         f: Coeff,
-        maxBitLen: Int,
+        maxFusedBitLen: Int,
         x1: Long, x0: Long,
         y1: Long, y0: Long,
         a3: Long, a2: Long, a1: Long, a0: Long
     ) {
         val pp00Hi = unsignedMultiplyHigh(x0, y0)
         val pp00Lo = x0 * y0
-        if (maxBitLen <= 64) {
+        if (maxFusedBitLen <= 64) {
             val f0 = pp00Lo + a0
             f.coeffSet64(f0)
             return
@@ -421,7 +421,7 @@ object CoeffFma {
         val pp01Lo = x0 * y1
         val pp10Hi = unsignedMultiplyHigh(x1, y0)
         val pp10Lo = x1 * y0
-        if (maxBitLen <= 128) {
+        if (maxFusedBitLen <= 128) {
             val f1 = carry0 + pp00Hi + pp01Lo + pp10Lo + a1
             f.coeffSet128(f1, f0)
             return
@@ -429,21 +429,21 @@ object CoeffFma {
         val (carry1, f1) = sumU64(carry0, pp00Hi, pp01Lo, pp10Lo, a1)
         val pp11Hi = unsignedMultiplyHigh(x1, y1)
         val pp11Lo = x1 * y1
-        if (maxBitLen <= 192) {
+        if (maxFusedBitLen <= 192) {
             val f2 = carry1 + pp01Hi + pp10Hi + pp11Lo + a2
             f.coeffSet192(f2, f1, f0)
             return
         }
         val (carry2, f2) = sumU64(carry1, pp01Hi, pp10Hi, pp11Lo, a2)
 
-        if (maxBitLen <= 256) {
+        if (maxFusedBitLen <= 256) {
             val f3 = carry2 + pp11Hi + a3
             f.coeffSet256(f3, f2, f1, f0)
             return
         }
         val (carry3, f3) = sumU64(carry2, pp11Hi, a3)
         if (carry3 == 0L) {
-            assert(maxBitLen in 257..258)
+            assert(maxFusedBitLen in 257..258)
             f.coeffSet256(f3, f2, f1, f0)
             return
         }
@@ -452,14 +452,14 @@ object CoeffFma {
 
     private fun _fma2x1x3(
         f: Coeff,
-        maxBitLen: Int,
+        maxFusedBitLen: Int,
         x1: Long, x0: Long,
         y0: Long,
         a2: Long, a1: Long, a0: Long
     ) {
         val pp00Hi = unsignedMultiplyHigh(x0, y0)
         val pp00Lo = x0 * y0
-        if (maxBitLen <= 64) {
+        if (maxFusedBitLen <= 64) {
             val f0 = pp00Lo + a0
             f.coeffSet64(f0)
             return
@@ -467,13 +467,13 @@ object CoeffFma {
         val (carry0, f0) = sumU64(pp00Lo, a0)
         val pp10Hi = unsignedMultiplyHigh(x1, y0)
         val pp10Lo = x1 * y0
-        if (maxBitLen <= 128) {
+        if (maxFusedBitLen <= 128) {
             val f1 = carry0 + pp00Hi + pp10Lo + a1
             f.coeffSet128(f1, f0)
             return
         }
         val (carry1, f1) = sumU64(carry0, pp00Hi, pp10Lo, a1)
-        if (maxBitLen <= 192) {
+        if (maxFusedBitLen <= 192) {
             val f2 = carry1 + pp10Hi + a2
             f.coeffSet192(f2, f1, f0)
             return
@@ -486,20 +486,20 @@ object CoeffFma {
     @Suppress("NOTHING_TO_INLINE")
     private inline fun _fma1x1x2(
         f: Coeff,
-        maxBitLen: Int,
+        maxFusedBitLen: Int,
         x0: Long,
         y0: Long,
         a1: Long, a0: Long
     ) {
         val pp00Hi = unsignedMultiplyHigh(x0, y0)
         val pp00Lo = x0 * y0
-        if (maxBitLen <= 64) {
+        if (maxFusedBitLen <= 64) {
             val f0 = pp00Lo + a0
             f.coeffSet64(f0)
             return
         }
         val (carry0, f0) = sumU64(pp00Lo, a0)
-        if (maxBitLen <= 128) {
+        if (maxFusedBitLen <= 128) {
             val f1 = carry0 + pp00Hi + a1
             f.coeffSet128(f1, f0)
             return
