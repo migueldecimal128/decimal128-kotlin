5061a3b49fa2b9784966dd833a4ff0d822140939
MiguelDecimal128 <miguel@decimal128.com>
2025-06-12 20:03:58 -0400

    sqrt works well, but questions about trailing zeros

--- src/test/kotlin/TestSqrt.kt
+++ src/test/kotlin/TestSqrt.kt
@@ -1,18 +1,35 @@
 package com.decimal128
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import java.lang.Long.numberOfTrailingZeros
+import java.lang.Math.unsignedMultiplyHigh
 import java.math.BigDecimal
+import java.math.BigInteger
 import java.math.MathContext
+import java.math.RoundingMode
+import java.util.*
 
 class TestSqrt{
 
     class TC(val bd: BigDecimal) {
         constructor(str: String) : this(BigDecimal(str))
-        val sqrt = bd.sqrt(MathContext.DECIMAL128)
+        val sqrt0 = bd.sqrt(MathContext.DECIMAL128)
+        val sqrt0Squared = sqrt0.multiply(sqrt0)
+        val isPerfect = sqrt0Squared.compareTo(bd) == 0
+        val zeroPadding = 34 - sqrt0.precision()
+        val sqrt = if (isPerfect) sqrt0 else sqrt0.setScale(sqrt0.scale() + zeroPadding, RoundingMode.UNNECESSARY)
     }
 
     val tcs = arrayOf (
+        TC("+10E-1839"),
+        TC("+0.0E4019"),
+        TC("+2139362027"),
+        TC("+2139362027E-4288"),
+        TC("1e1"),
         TC("1"),
+        TC("2"),
+        TC("10"),
         TC("4"),
         TC("16"),
         TC("256"),
@@ -23,6 +40,7 @@ class TestSqrt{
         TC("625"),
         TC("900"),
         TC("10000"),
+        TC("1234567890123456789012345678901234"),
     )
 
     @Test
@@ -31,22 +49,53 @@ class TestSqrt{
             test1(tc)
     }
 
+    @Test
+    fun testProblemChild() {
+        val tc = TC("+10E-1839")
+        test1(tc)
+    }
+
+    @Test
+    fun testRandom() {
+        for (i in 0..<100000) {
+            val tc = TC(randBd())
+            test1(tc)
+        }
+
+    }
+
+    val random = Random()
+
+    fun randBd() : BigDecimal {
+        val bitLength = random.nextInt(0, 112)
+        val bi = BigInteger(bitLength, random)
+        val exp = random.nextInt(3*4096) - 6176
+        val bd = BigDecimal(bi).scaleByPowerOfTen(exp)
+        return bd
+    }
+
     fun test1(tc: TC) {
         val dec = Dec34()
         val decSqrt = Dec34()
         dec.set(tc.bd)
         setSqrt(decSqrt, dec)
+        val expected = tc.sqrt
+        assertEquals(expected.unscaledValue(), decSqrt.coeffToBigInteger())
+        assertEquals(-expected.scale(), decSqrt.qExp)
     }
 
     fun setSqrt(sqrt: Dec34, radicand: Dec34) {
         val qPreferred = radicand.qExp shr 1
         if (radicand.coeffIsZero()) {
+            val sciExp = radicand.sciExp()
+            println("zero radicand:$radicand sciExp:$sciExp")
             sqrt.setZero()
             sqrt.qExp = qPreferred
+            sqrt.sign = radicand.sign
+            return
         }
         val radicandScaled = Coeff()
-        val scaleUp = 66 - radicand.digitLen + (radicand.digitLen and 1)
-        //val scaleUp = 12
+        val scaleUp = 70 - radicand.digitLen + (radicand.digitLen and 1) + (radicand.qExp and 1)
         radicandScaled.coeffSetScaleUpPow10(radicand, scaleUp)
         println("radicand:$radicand radicandScaled:$radicandScaled")
 
@@ -64,10 +113,14 @@ class TestSqrt{
         guess0Coeff.coeffSetShiftLeft(guess0Coeff, Math.max(guess0Exp - 52, 0))
         println(" --> dbl0:$dbl0 doubleGuess0:$guess0Double coeffGuess0:$guess0Coeff")
 
-        val guess0squared = Coeff()
-        guess0squared.coeffSetMul(guess0Coeff, guess0Coeff)
+        val guess0Squared = Coeff()
+        guess0Squared.coeffSetMul(guess0Coeff, guess0Coeff)
+        while (guess0Squared.coeffUnscaledCompareTo(radicandScaled) > 0) {
+            guess0Coeff.coeffMutateDecrement()
+            guess0Squared.coeffSetMul(guess0Coeff, guess0Coeff)
+        }
         val residual0 = Coeff()
-        CoeffSub.coeffSubUnscaled(residual0, radicandScaled, guess0squared)
+        CoeffSub.coeffSubUnscaled(residual0, radicandScaled, guess0Squared)
         println(" --> residual0:$residual0")
 
         val residual0BitCount = Math.min(53, residual0.bitLen)
@@ -87,20 +140,83 @@ class TestSqrt{
         guess1Coeff.coeffSetAdd(guess0Coeff, delta0Coeff)
         println(" --> guess1Coeff:$guess1Coeff")
 
-        val guess1squared = Coeff()
-        guess1squared.coeffSetMul(guess1Coeff, guess1Coeff)
+        val guess1Squared = Coeff()
+        guess1Squared.coeffSetMul(guess1Coeff, guess1Coeff)
+        while (guess1Squared.coeffUnscaledCompareTo(radicandScaled) > 0) {
+            guess1Coeff.coeffMutateDecrement()
+            guess1Squared.coeffSetMul(guess1Coeff, guess1Coeff)
+        }
         val residual1 = Coeff()
-        CoeffSub.coeffSubUnscaled(residual1, radicandScaled, guess1squared)
+        CoeffSub.coeffSubUnscaled(residual1, radicandScaled, guess1Squared)
         println(" --> residual1:$residual1")
 
         val guess1x2 = Coeff()
         guess1x2.coeffSetShiftLeft(guess1Coeff, 1)
         val delta1 = Coeff()
-        val residue = delta1.coeffSetDiv(residual1, guess1x2)
+        val residue1 = delta1.coeffSetDiv(residual1, guess1x2)
 
         val guess2 = Coeff()
         guess2.coeffSetAdd(guess1Coeff, delta1)
         println(" --> guess2:$guess2")
+
+        val guess2Squared = Coeff()
+        guess2Squared.coeffSetMul(guess2, guess2)
+        val residual2 = Coeff()
+        CoeffSub.coeffSubUnscaled(residual2, radicandScaled, guess2Squared)
+        println(" --> residual2:$residual2")
+
+        println(" --> scaleUp:$scaleUp preferred:$qPreferred")
+
+        sqrt.coeffSet(guess2)
+        sqrt.qExp = -scaleUp / 2
+        sqrt.sign = 0
+
+        println(" --> sqrt:$sqrt")
+
+        val residue2 = if (residual2.coeffIsZero()) Residue.EXACT else Residue.LT_HALF
+        var qZ = (radicand.qExp - scaleUp) / 2
+        var ntz = numberOfTrailingZeros(sqrt.dw0)
+        if (residue2 == Residue.EXACT && qZ < qPreferred && ntz > 0) {
+            if (qZ + 1 < qPreferred) {
+                val quot = Coeff()
+                do {
+                    val deltaQ = qPreferred - qZ
+                    val chunk = Math.min(Math.min(9, deltaQ), ntz)
+                    val chunkRemainder = DivBarrett.barrettDivModPow10(quot, sqrt, chunk)
+                    if (chunkRemainder > 0) {
+                        var pow10Count = 0
+                        var t = chunkRemainder
+                        val M = 0xCCCCCCCCCCCCCCCDuL.toLong()
+                        while (true) {
+                            // val q = t / 10
+                            // val r = t % 10
+                            val q = unsignedMultiplyHigh(t, M) ushr 3
+                            val r = t - (q * 10)
+                            if (r != 0L)
+                                break
+                            ++pow10Count
+                            t = q
+                        }
+                        if (pow10Count > 0) {
+                            sqrt.coeffSetScaleDownPow10(sqrt, pow10Count)
+                            qZ += pow10Count
+                        }
+                        break
+                    } else {
+                        sqrt.coeffSet(quot)
+                        ntz -= chunk
+                        qZ += chunk
+                    }
+                } while (qZ < qPreferred && ntz > 0)
+            } else if (sqrt.coeffIsMultipleOf10()) {
+                sqrt.coeffSetScaleDownPow10(sqrt, 1)
+                ++qZ
+            }
+        }
+        sqrt.qExp = qZ
+        sqrt.roundAndFinalize(residue2, 0, Decimal128Context())
+
+        println(" --> sqrt:$sqrt")
 
         /*
         val bitCount1 = Math.min(53, coeff0Diff.bitLen)
