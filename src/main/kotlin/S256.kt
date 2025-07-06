5b942ce487f19703aba05342ea60482a08c0faad
MiguelDecimal128 <miguel@decimal128.com>
2025-07-06 09:26:57 -0400

    Int256 wrapper ... untested

--- src/main/kotlin/S256.kt
+++ src/main/kotlin/S256.kt
@@ -2,12 +2,52 @@ package com.decimal128
 
 import com.decimal128.U256Compare.u256UnscaledCompare
 
-open class S256() : U256() {
-    var sign = false
+@Suppress("NOTHING_TO_INLINE")
+open class S256 : U256 {
+    constructor(): super()
+    constructor(x: S256): super() {
+        s256Set(x)
+    }
+    constructor(sign: Boolean, dw3: Long, dw2: Long, dw1: Long, dw0: Long): super(dw3, dw2, dw1, dw0) {
+        s256Set(sign, dw3, dw2, dw1, dw0)
+        this.sign = sign
+    }
+    constructor(str: String) : super() {
+        Int256ParsePrint.u256FromString(this, str)
+    }
 
-    fun s256Add(x: S256, y: S256) = s256AddImpl(x, y.sign, y)
+    @JvmField
+    internal var sign = false
 
-    fun s256Sub(x: S256, y: S256) = s256AddImpl(x, ! y.sign, y)
+    fun s256SetZero() {
+        dw3 = 0L; dw2 = 0L; dw1 = 0L; dw0 = 0L;
+        bitLen = 0; digitLen = 0;
+        sign = false
+    }
+
+    fun s256SetSigned(n: Long) {
+        if (n >= 0)
+            s256Set(false, 0L, 0L, 0L, n)
+        else
+            s256Set(true, 0L, 0L, 0L, -n) // this works for Long.MIN_VALUE too
+    }
+
+    fun s256SetUnsigned(u: Long) = s256Set(false, 0L, 0L, 0L, u)
+
+    fun s256Set(x: S256) {
+        dw3 = x.dw3; dw2 = x.dw2; dw1 = x.dw1; dw0 = x.dw0
+        bitLen = x.bitLen; digitLen = x.digitLen
+        sign = x.sign
+    }
+
+    fun s256Set(sign: Boolean, dw3: Long, dw2: Long, dw1: Long, dw0: Long) {
+        u256Set256(dw3, dw2, dw1, dw0)
+        this.sign = sign and (bitLen > 0)
+    }
+
+    internal inline fun s256Add(x: S256, y: S256) = s256AddImpl(x, y.sign, y)
+
+    internal inline fun s256Sub(x: S256, y: S256) = s256AddImpl(x, ! y.sign, y)
 
     private fun s256AddImpl(x: S256, ySign: Boolean, y: S256) {
         val xSign = x.sign
@@ -33,40 +73,40 @@ open class S256() : U256() {
         }
     }
 
-    fun s256Mul(x: S256, y: S256) {
+    internal inline fun s256Mul(x: S256, y: S256) {
         this.u256SetMul(x, y)
         this.sign = (x.sign xor y.sign) and (bitLen > 0)
     }
 
-    fun s256DivX64(x: S256, y0: Long): Residue {
+    internal inline fun s256DivX64(x: S256, y0: Long): Residue {
         val residue = this.u256SetDivX64(x, y0)
         this.sign = x.sign and (bitLen > 0)
         return residue
     }
 
-    fun s256Div(x: S256, y: S256): Residue {
+    internal inline fun s256Div(x: S256, y: S256): Residue {
         val residue = this.u256SetDiv(x, y)
         this.sign = (x.sign xor y.sign) and (bitLen > 0)
         return residue
     }
 
-    fun s256Mod(x: S256, y: S256) {
+    internal inline fun s256Mod(x: S256, y: S256) {
         this.u256SetMod(x, y)
         this.sign = (x.sign xor y.sign) and (bitLen > 0)
     }
 
-    fun s256SetScaleUpPow10(x: S256, pow10: Int) {
+    internal inline fun s256SetScaleUpPow10(x: S256, pow10: Int) {
         this.u256SetScaleUpPow10(x, pow10)
-        this.sign = x.sign
+        this.sign = x.sign and (bitLen > 0)
     }
 
-    fun s256ScaleDownPow10(x: S256, pow10: Int): Residue {
+    internal inline fun s256ScaleDownPow10(x: S256, pow10: Int): Residue {
         val residue = this.u256SetScaleDownPow10(x, pow10)
-        this.sign = x.sign
+        this.sign = x.sign and (bitLen > 0)
         return residue
     }
 
-    fun s256SetFmaPow10(x: S256, pow10: Int, a: S256) {
+    internal inline fun s256SetFmaPow10(x: S256, pow10: Int, a: S256) {
         if (! (x.sign xor a.sign)) {
             u256SetFmaPow10(x, pow10, a)
             this.sign = x.sign and (bitLen > 0)
@@ -78,6 +118,35 @@ open class S256() : U256() {
     }
 
     override fun toString() = Int256ParsePrint.int256ToString(sign, this)
+
+    fun s256UnscaledCompareTo(other: S256): Int {
+        if (bitLen > 0) {
+            if (other.bitLen > 0) {
+                if (! this.sign) {
+                    if (other.sign)
+                        return 1
+                    else
+                        return u256UnscaledCompareTo(other)
+                } else {
+                    if (other.sign)
+                        return -u256UnscaledCompareTo(other)
+                    else
+                        return 1
+                }
+            } else {
+                // other == 0
+                return if (! sign) 1 else -1
+            }
+        } else {
+            // this == 0
+            if (other.bitLen > 0) {
+                return if (other.sign) 1 else -1
+            } else {
+                // other == 0
+                return 0
+            }
+        }
+    }
 
     override fun equals(other: Any?) = other is S256 && sign == other.sign && u256UnscaledEQ(other)
 
