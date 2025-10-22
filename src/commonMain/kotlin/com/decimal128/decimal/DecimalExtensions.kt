b0fb5077ae28ec5c2434a1c987105c673db35dc0
MiguelDecimal128 <miguel@decimal128.com>
2025-10-22 14:17:55 -0400

    minor pruning

--- src/commonMain/kotlin/com/decimal128/decimal/DecimalExtensions.kt
+++ src/commonMain/kotlin/com/decimal128/decimal/DecimalExtensions.kt
@@ -1,8 +1,3 @@
 package com.decimal128.decimal
 
 
-operator fun MutDec.plus(other: MutDec) = this.add(other)
-operator fun MutDec.minus(other: MutDec) = this.subtract(other)
-operator fun MutDec.times(other: MutDec) = this.multiply(other)
-operator fun MutDec.div(other: MutDec) = this.divide(other)
-
