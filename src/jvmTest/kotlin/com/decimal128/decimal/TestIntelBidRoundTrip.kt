d27cbb3acad80e6d1ebe80fb2361ff4796e1f8b4
MiguelDecimal128 <miguel@decimal128.com>
2025-11-27 20:29:29 -0500

    added round trip test for parsing Intel hex format

---
+++ src/jvmTest/kotlin/com/decimal128/decimal/TestIntelBidRoundTrip.kt
@@ -0,0 +1,43 @@
+package com.decimal128.decimal
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestIntelBidRoundTrip {
+
+    val verbose = true
+
+    val tcs = arrayOf(
+        "[0001ed09bead87c0378d8e62ffffffff]",
+        "[78000000000000000000000000000000]", // Infinity
+        "[7c000000000000000000000000000000]", // qNaN
+        "[7e000000000000000000000000000000]", // sNaN
+        "[00000000000000000000000000000000]",
+        "[2ffe4d723cabcb53dd5f2ab27379cfc8]",
+        "[ab5b7f8969162c5f,9951aecf3b28ba61]",
+        "[fe001538549b96bc,d8bac0361145a524]",
+        "[2FFC7C94BB6248E89D4E9D197FA236D6]",
+    )
+
+    @Test
+    fun testCases() {
+        for (tc in tcs)
+            test1(tc)
+    }
+
+    fun test1(tc: String) {
+        if (verbose)
+            println("tc:$tc")
+        val (isValid, dw1, dw0) = DecSerdeBid128.parseIntelBidHex(tc)
+        assertTrue(isValid)
+        val decimal = DecSerdeBid128.decodeBid128(dw1, dw0)
+        val roundTrip = LongArray(2)
+        DecSerdeBid128.encodeBid128(decimal, roundTrip, isLittleEndian = false)
+        assertEquals(dw1, roundTrip[0].toULong())
+        assertEquals(dw0, roundTrip[1].toULong())
+        DecSerdeBid128.encodeBid128(decimal, roundTrip, isLittleEndian = true)
+        assertEquals(dw0, roundTrip[0].toULong())
+        assertEquals(dw1, roundTrip[1].toULong())
+    }
+}
