9e7bf097c1daa4b053ac1af7f2d0e078532faff8
MiguelDecimal128 <miguel@decimal128.com>
2026-02-22 02:30:27 -0500

    Intel bid128_rem cases ... those with large difference in qExp fail because my one-step implementation is inadequate. remainderNear/Truncate needs work!

--- src/jvmTest/kotlin/com/decimal128/decimal/intel/TestBid128BasicArithmetic.kt
+++ src/jvmTest/kotlin/com/decimal128/decimal/intel/TestBid128BasicArithmetic.kt
@@ -4,6 +4,11 @@ import com.decimal128.decimal.DecContext
 import com.decimal128.decimal.DecPrefs
 import com.decimal128.decimal.Decimal
 import com.decimal128.decimal.addImpl
+import com.decimal128.decimal.divImpl
+import com.decimal128.decimal.mulImpl
+import com.decimal128.decimal.remImpl
+import com.decimal128.decimal.remNearImpl
+import com.decimal128.decimal.remTruncImpl
 import com.decimal128.decimal.subImpl
 import kotlin.test.Test
 
@@ -64,6 +69,84 @@ class TestBid128BasicArithmetic {
             "bid128_sub 0 +1000000010000010.0100000E-6192 +8998898888898999998899999999.988888889E-6026 [8121bbae128738e463d45adcde9f1499] 00",
         ),
         ::subImpl,
+        decContext = decContext,
+        verbose = verbose,
+    )
+
+    @Test
+    fun testMul() = IntelRunner1.runBinaryDecimalOp(
+        "/intel/readtest.in",
+        "bid128_mul",
+        ::mulImpl,
+        skip = true,
+        skipCases = arrayOf(
+        ),
+        decContext = decContext,
+        verbose = verbose
+    )
+
+    @Test
+    fun testMulCases() = IntelRunner1.runBinaryDecimalOp(
+        arrayOf(
+        ),
+        ::mulImpl,
+        decContext = decContext,
+        verbose = verbose,
+    )
+
+    @Test
+    fun testDiv() = IntelRunner1.runBinaryDecimalOp(
+        "/intel/readtest.in",
+        "bid128_div",
+        ::divImpl,
+        skip = true,
+        skipCases = arrayOf(
+        ),
+        decContext = decContext,
+        verbose = verbose
+    )
+
+    @Test
+    fun testDivCases() = IntelRunner1.runBinaryDecimalOp(
+        arrayOf(
+        ),
+        ::divImpl,
+        decContext = decContext,
+        verbose = verbose,
+    )
+
+    @Test
+    fun testRemNear() = IntelRunner1.runBinaryDecimalOp(
+        "/intel/readtest.in",
+        "bid128_rem",
+        ::remNearImpl,
+        skip = true,
+        skipCases = arrayOf(
+            // FIXME - these are valid ... my implementation is not :(
+            // FIXME - the case of large qDelta must be handled differently
+            "bid128_rem 0 [26741c0590811072,ffffffff6ef7ffff] [0000000000000000,0300040220100840] [8000000000000000015515ab6c6033c0] 00",
+            "bid128_rem 0 [397cdc9a254ff562,99bc2af7b316f57c] [268d807293a0dcac,afddbd631e831cbb] [a68c1d3c3991cbb19eee601c8bdcb12c] 00",
+            "bid128_rem 0 [4082000215010400,1f7dbeffb7fdff7d] [bfffbfff7ffa7fbf,240c30b8380ad037] [bffe0708356252ac4b444f585a0c6d93] 00",
+            "bid128_rem 0 [97f3ca2bca3dcd34,321ba8822721f58f] [8000000000000000,bae106a1402505d6] [000000000000000025bf9a7041265ffe] 00",
+            "bid128_rem 0 [9c5f9589d6c5fa51,caa6dcb43a12852e] [0080000000000000,0001000000000000] [80800000000000000000000000000000] 00",
+            "bid128_rem 0 [a7f734b7f7e7a516,229ddcc454df042c] [0000000000000002,9fc80ebfc7fed6d1] [800000000000000118eb45cf8f8cc645] 00",
+            "bid128_rem 1 [397cdc9a254ff562,99bc2af7b316f57c] [268d807293a0dcac,afddbd631e831cbb] [a68c1d3c3991cbb19eee601c8bdcb12c] 00",
+            "bid128_rem 2 [397cdc9a254ff562,99bc2af7b316f57c] [268d807293a0dcac,afddbd631e831cbb] [a68c1d3c3991cbb19eee601c8bdcb12c] 00",
+            "bid128_rem 3 [397cdc9a254ff562,99bc2af7b316f57c] [268d807293a0dcac,afddbd631e831cbb] [a68c1d3c3991cbb19eee601c8bdcb12c] 00",
+            "bid128_rem 4 [397cdc9a254ff562,99bc2af7b316f57c] [268d807293a0dcac,afddbd631e831cbb] [a68c1d3c3991cbb19eee601c8bdcb12c] 00",
+        ),
+        decContext = decContext,
+        verbose = verbose
+    )
+
+    @Test
+    fun testRemNearCases() = IntelRunner1.runBinaryDecimalOp(
+        arrayOf(
+            // FIXME - the case of large qDelta must be handled differently
+            // "bid128_rem 0 [26741c0590811072,ffffffff6ef7ffff] [0000000000000000,0300040220100840] [8000000000000000015515ab6c6033c0] 00",
+            // "bid128_rem 0 [397cdc9a254ff562,99bc2af7b316f57c] [268d807293a0dcac,afddbd631e831cbb] [a68c1d3c3991cbb19eee601c8bdcb12c] 00",
+        ),
+        ::remNearImpl,
         decContext = decContext,
         verbose = verbose,
     )
