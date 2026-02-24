7451014e5f3f3ea60aefabf689e12ff679c56d5e
MiguelDecimal128 <miguel@decimal128.com>
2026-02-24 16:40:20 -0500

    more intel comparison tests

--- src/jvmTest/kotlin/com/decimal128/decimal/intel/TestBid128Comparison.kt
+++ src/jvmTest/kotlin/com/decimal128/decimal/intel/TestBid128Comparison.kt
@@ -37,10 +37,147 @@ class TestBid128Comparison {
 
     @Test
     fun testCompareQuietLess() = IntelRunner1.runBinaryBooleanCtxMethodOp(
-            "/intel/readtest.in",
-            "bid128_quiet_less",
-            Decimal::compareQuietLess,
-            DecContext.DECIMAL128,
-            verbose = verbose
-        )
+        "/intel/readtest.in",
+        "bid128_quiet_less",
+        Decimal::compareQuietLess,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareQuietLessEqual() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_quiet_less_equal",
+        Decimal::compareQuietLessEqual,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareQuietLessUnordered() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_quiet_less_unordered",
+        Decimal::compareQuietLessUnordered,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareQuietNotEqual() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_quiet_not_equal",
+        Decimal::compareQuietNotEqual,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareQuietNotGreater() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_quiet_not_greater",
+        Decimal::compareQuietNotGreater,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareQuietNotLess() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_quiet_not_less",
+        Decimal::compareQuietNotLess,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareQuietOrdered() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_quiet_ordered",
+        Decimal::compareQuietOrdered,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareQuietUnordered() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_quiet_unordered",
+        Decimal::compareQuietUnordered,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingGreater() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_greater",
+        Decimal::compareSignalingGreater,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingGreaterEqual() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_greater_equal",
+        Decimal::compareSignalingGreaterEqual,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingGreaterUnordered() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_greater_unordered",
+        Decimal::compareSignalingGreaterUnordered,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingLess() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_less",
+        Decimal::compareSignalingLess,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingLessEqual() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_less_equal",
+        Decimal::compareSignalingLessEqual,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingLessUnordered() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_less_unordered",
+        Decimal::compareSignalingLessUnordered,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingNotGreater() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_not_greater",
+        Decimal::compareSignalingNotGreater,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+    @Test
+    fun testCompareSignalingNotLess() = IntelRunner1.runBinaryBooleanCtxMethodOp(
+        "/intel/readtest.in",
+        "bid128_signaling_not_less",
+        Decimal::compareSignalingNotLess,
+        DecContext.DECIMAL128,
+        verbose = verbose
+    )
+
+
 }
