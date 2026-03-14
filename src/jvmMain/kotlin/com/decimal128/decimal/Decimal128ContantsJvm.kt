291716904a03b305c3818b78eb6c471d5b1c43dc
MiguelDecimal128 <miguel@decimal128.com>
2026-03-14 06:24:23 -0400

    POW10 alias is now a static @JvmField

--- src/jvmMain/kotlin/com/decimal128/decimal/Decimal128ContantsJvm.kt
+++ src/jvmMain/kotlin/com/decimal128/decimal/Decimal128ContantsJvm.kt
@@ -1,4 +1,8 @@
 package com.decimal128.decimal
+
+// FIXME - for some reason I cannot have both DWORD_TABLES and the alias
+//  POW10 as expect/actual
+//  Some unit tests fail, but if I run the tests individually then they run fine.
 /*
 @JvmField
 actual val DWORD_TABLES: LongArray = LongArray(1024)
