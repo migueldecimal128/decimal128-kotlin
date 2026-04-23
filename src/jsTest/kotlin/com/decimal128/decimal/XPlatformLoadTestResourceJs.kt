4892b63836908f3d58f0f345d74873253aba0f76
MiguelDecimal128 <miguel@decimal128.com>
2026-04-23 16:12:51 -0400

    intel and dectest test vectors now run on Node.js

--- src/jsTest/kotlin/com/decimal128/decimal/XPlatformLoadTestResourceJs.kt
+++ src/jsTest/kotlin/com/decimal128/decimal/XPlatformLoadTestResourceJs.kt
@@ -1,3 +1,13 @@
 package com.decimal128.decimal
 
-actual fun loadTestResourceAsString(fileName: String): String? = null
+actual fun loadTestResourceAsString(fileName: String): String? {
+    val cwd = js("process.cwd()") as String
+    val path = "$cwd/../../../../src/jvmTest/resources$fileName"
+    return try {
+        val fs = js("require('fs')")
+        fs.readFileSync(path, "utf8") as String
+    } catch (_: Throwable) {
+        null
+    }
+}
+
